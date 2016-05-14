bb30909f6259dbb69e3fb6d5beeb422a1a47ff66
flexilef <iflex606@gmail.com>
2016-05-13 18:00:48 -0700

    Implemented all parser functions except for parseFactor() rule. The Rules with epsilon include cheats... might need to refactor in the future: parseTermFactored() and parseExpressionFactored. The Language Grammar has also been updated.

--- src/interpreter/Parser.java
+++ src/interpreter/Parser.java
@@ -23,11 +23,12 @@ public class Parser {
     tokens.add(new Token("BasicType", "int"));
     tokens.add(new Token("Identifier", "var"));
     tokens.add(new Token("Terminal", "="));
-    //tokens.add(new Token("LiteralInteger", "5"));
-    tokens.add(new Token("Identifier", "abc"));
+    tokens.add(new Token("LiteralInteger", "5"));
+    //tokens.add(new Token("Identifier", "abc"));
     //tokens.add(new Token("LiteralInteger", "1"));
-    //tokens.add(new Token("BinaryOperator", "+"));
+    tokens.add(new Token("BinaryOperator", "+"));
     //tokens.add(new Token("LiteralInteger", "2"));
+    tokens.add(new Token("Identifier", "cde"));
     tokens.add(new Token("Terminal", ";"));
 
     tokenStream = new TokenStream(tokens);
@@ -69,6 +70,12 @@ public class Parser {
     currentToken = tokenStream.getNext();
     if (!parseAssignment()) {
       System.out.println("Error! parseInitialization: parseAssignment");
+
+      //pushBack previous token
+      tokenStream.pushBack(1);
+      //set current token to the previous token
+      currentToken = tokenStream.getNext();
+
       return false;
     }
 
@@ -77,29 +84,60 @@ public class Parser {
 
   public boolean parseAssignment() {
 
-    /* First rule */
     if (!parseIdentifier()) {
       return false;
     }
 
-    currentToken = tokenStream.getNext();
+    if (tokenStream.hasNext()) {
+      currentToken = tokenStream.getNext();
+    } else {
+      return false;
+    }
     if (!parseEqualSymbol()) {
       System.out.println("Error! parseAssignment: parseEqualSymbol");
+
+      //pushBack previous token
+      tokenStream.pushBack(1);
+      //set current token to the previous token
+      currentToken = tokenStream.getNext();
+
       return false;
     }
 
     currentToken = tokenStream.getNext();
     if (!parseExpression()) {
-      if (!parseLiteralInteger()) {
-        if (!parseIdentifier()) {
-          return false;
-        }
-      }
+      //pushBack previous 2 tokens
+      tokenStream.pushBack(2);
+      //set current token to the previous token
+      currentToken = tokenStream.getNext();
+
+      return false;
     }
+    /*
+     currentToken = tokenStream.getNext();
+     if (!parseExpression()) {
+     if (!parseLiteralInteger()) {
+     if (!parseIdentifier()) {
+     //pushBack previous 2 tokens
+     tokenStream.pushBack(2);
+     //set current token to the previous token
+     currentToken = tokenStream.getNext();
+
+     return false;
+     }
+     }
+     }
+     */
 
     currentToken = tokenStream.getNext();
     if (!parseSemicolon()) {
       System.out.println("Error! parseAssignment: parseSemicolon");
+
+      //pushBack previous token
+      tokenStream.pushBack(1);
+      //set current token to the previous token
+      currentToken = tokenStream.getNext();
+
       return false;
     }
 
@@ -113,18 +151,44 @@ public class Parser {
     }
 
     currentToken = tokenStream.getNext();
-    if (!parsePlusSymbol()) {
-      //pushBack previous token
+    if (!parseExpressionFactored()) {
+      //pushback one tokens
       tokenStream.pushBack(1);
-      //set current token to the previous token
+      //set currentToken to next token
       currentToken = tokenStream.getNext();
       return false;
     }
-    
+
+    return true;
+  }
+
+  public boolean parseExpressionFactored() {
+
+    //epsilon
+    if (tokenStream.hasNext()) {
+      if (!parsePlusSymbol()) {
+        if (!parseMinusSymbol()) {
+          return false;
+        }
+      }
+    } else {
+      //token push back last token because we are at the end
+      //pushback one token
+      tokenStream.pushBack(1);
+      //set currentToken to next token
+      currentToken = tokenStream.getNext();
+
+      //found epsilon
+      return true;
+    }
+
     currentToken = tokenStream.getNext();
     if (!parseExpression()) {
-      //pushback two tokens
+      //pushback one token
+      tokenStream.pushBack(1);
       //set currentToken to next token
+      currentToken = tokenStream.getNext();
+
       return false;
     }
 
@@ -132,34 +196,83 @@ public class Parser {
   }
 
   public boolean parseTerm() {
-    
+
     if (!parseFactor()) {
       return false;
     }
 
-    /*if (parseMultiplySymbol()) {
+    if (tokenStream.hasNext()) {
+      currentToken = tokenStream.getNext();
     } else {
       return false;
     }
+    if (!parseTermFactored()) {
+      //pushBack previous token
+      tokenStream.pushBack(1);
+      //set current token to the previous token
+      currentToken = tokenStream.getNext();
 
-    if (parseTerm()) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public boolean parseTermFactored() {
+
+    //cheating: to apply the epsilon rule, we cheat knowing that 
+    //if term were replaced by factor (because of epsilon),
+    //then the following symbols can follow term/factor:
+    //; if Expression -> Term ;
+    //+ or - Term -> ExpressionFactored -> + | -
+    if (currentToken.getValue().equals("+")
+            || currentToken.getValue().equals("-")
+            || currentToken.getValue().equals(";")) {
+
+      //push back one token because we didn't consume it, we just peeked
+      tokenStream.pushBack(1);
+      currentToken = tokenStream.getNext();
+
       return true;
-    }*/
+    }
+
+    if (!parseMultiplySymbol()) {
+      if (!parseDivideSymbol()) {
+        return false;
+      }
+    }
+
+    currentToken = tokenStream.getNext();
+    if (!parseTerm()) {
+      //pushBack previous token
+      tokenStream.pushBack(1);
+      //set current token to the previous token
+      currentToken = tokenStream.getNext();
+
+      return false;
+    }
 
     return true;
   }
 
   public boolean parseFactor() {
-    
+
     if (!parseLiteralInteger()) {
-      return false;
+      if (!parseIdentifier()) {
+        return false;
+      }
+      return true;
     }
 
-    //second rule
-    //if(parseIdentifier()) {
-    //  return true;
-    //}
     return true;
+  }
+
+  public boolean parseDivideSymbol() {
+    if (currentToken.getValue().equals("/")) {
+      return true;
+    }
+
+    return false;
   }
 
   public boolean parseMultiplySymbol() {
@@ -172,6 +285,14 @@ public class Parser {
 
   public boolean parsePlusSymbol() {
     if (currentToken.getValue().equals("+")) {
+      return true;
+    }
+
+    return false;
+  }
+
+  public boolean parseMinusSymbol() {
+    if (currentToken.getValue().equals("-")) {
       return true;
     }
 
